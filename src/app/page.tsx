a444b9350d3952bbee794ce99ecf28ea07c48420
roman <roman@MacBook-Air-roman.local>
2024-03-07 15:30:36 +0300

    final version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import { HeaderImageBlock, FirstSection, SecondSection } from "@/components";
 
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex flex-col w-screen h-full text-blue bg-gradient-background box-border">
+    <main className="flex flex-col w-full h-full text-blue bg-gradient-background box-border">
       <HeaderImageBlock />
-      <div className="max-w-screen-wrapper w-full m-auto shadow-wrapper bg-white mt-[141px] mb-[117px]">
+      <div className="max-w-screen-wrapper flex flex-col w-full m-auto shadow-wrapper bg-white mt-[141px] mb-[117px]">
         <FirstSection />
         <SecondSection />
       </div>
